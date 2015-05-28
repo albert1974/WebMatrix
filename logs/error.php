257cc7654141b879e466cabc0433162ed6e8b6a5
albert1974 <albert4dev@gmail.com>
2015-05-28 16:54:40 +0300

    publish1

---
+++ logs/error.php
@@ -0,0 +1,11 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2015-05-28 13:06:33 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2015-05-28T13:06:33+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2015-05-28T13:06:57+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2015-05-28T13:07:29+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2015-05-28T13:20:54+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2015-05-28T13:21:02+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
